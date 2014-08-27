563283d0b39476fdd7f7776197f08d5ceb0e888c
sistemas <sergio.pernas@gmail.com>
2014-08-26 23:05:54 -0300

    sender

--- scripts/sender.sh
+++ scripts/sender.sh
@@ -5,7 +5,7 @@ echo -n 'user: '
 read user
 echo -n 'group: '
 read group
-socat -d -d PTY,link=/dev/ttyS10,raw,echo=0,crnl,user=$user,group=$group PTY,link=/dev/ttyS11,raw,echo=0,crnl,user=$user,group=$group &
+socat -d -d PTY,link=/dev/ttyAMA0,raw,echo=0,crnl,user=$user,group=$group PTY,link=/dev/AMA1,raw,echo=0,crnl,user=$user,group=$group &
 
 
 
@@ -26,7 +26,7 @@ echo "
 rs[sn]=AQM120817&rs[ts]=$timestamp&rs[lat]=N 89 60.0000&rs[lon]=E 000 00.0000&rs[si1]=Temp&rs[sv1]=$sen.0000&rs[si2]=Hum&rs[sv2]=63.0000&rs[si3]=CO&rs[sv3]=$sen.0000&rs[si4]=SO2&rs[sv4]=$sen.0000&rs[si5]=VOCs&rs[sv5]=$sen.0000&rs[obs]=Generado en laboratorio el dia $timestamp&rs[si6]=Power&rs[sv6]=I/O&rs[si7]=Bat&rs[sv7]=63.0000
 -I- Enviando Cmd de 51 char: $PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29
 -I- Modulo GPS reseteado.
--I- Ventiladores encendidos." | tee /dev/ttyS10
+-I- Ventiladores encendidos." | tee /dev/ttyAMA0
 
 
 done
